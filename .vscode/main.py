f122b97433a56c3e2d819511f7ec817e4570b2c4
Hanjishi <dalisay.john06@gmail.com>
2024-12-14 22:02:44 +0800

    Delete .vscode directory

--- .vscode/main.py
+++
@@ -1,9 +0,0 @@
-def findUserbyEmail(email):
-    if(email -- 'progvar@gmail.com'):
-        return{
-            "email": "progvar@gmail.com",
-            "password":"progvar123"
-        }
-
-
-print(findUserbyEmail)
